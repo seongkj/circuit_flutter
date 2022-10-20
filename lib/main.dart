e3d069e7c91a6a66bc39a50c6992c9b6ea8b620c
seongkj <yesican1011@naver.com>
2022-10-20 15:34:12 +0900

    flutter 카카오 채팅 clone

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,138 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('test'),
-        ),
-        body: Container(
-          color: Colors.amber[100],
-          height: 100,
-          padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-          child: Row(
-            children: [
-              Image.asset('img1.png', width: 150),
-              Container(
-                width: 100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        body: MyTest(),
+        bottomNavigationBar: MyBanner(),
+      ),
+    );
+  }
+}
+
+class MyTest extends StatelessWidget {
+  const MyTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Container(
+            // 네비 바
+            color: Colors.black12,
+            height: double.infinity,
+            width: 80,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('asdf'),
-                    Text('asdfsss'),
-                    Text('asdf'),
-                    Text('asdfs'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Icon(Icons.favorite), Text('123')],
-                    )
+                    Container(
+                      padding: EdgeInsets.only(top: 30),
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Icon(Icons.person, size: 35),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 30),
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Icon(Icons.chat_bubble_outline, size: 35),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 30),
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Icon(Icons.more_horiz, size: 35),
+                      ),
+                    ),
                   ],
                 ),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 30),
+                      child: Icon(Icons.notifications_none, size: 35),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 30),
+                      child: Icon(Icons.settings, size: 35),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 400,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '채팅',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.search, size: 30),
+                          Icon(Icons.forum, size: 30),
+                          Icon(Icons.add_comment, size: 30),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Text('목록'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 광고배너
+class MyBanner extends StatelessWidget {
+  const MyBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: SizedBox(
+          height: 150,
+          child: Row(
+            children: [
+              Image(image: AssetImage('img1.png')),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    '여기는 광고배너',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    '브라운 이미지',
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
