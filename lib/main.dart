96252a1ca1e63a55de918a54a6737d709c0f001e
성경주 <59130927+seongkj@users.noreply.github.com>
2022-10-22 15:37:52 +0900

    flutter SingleChildScrollView 스크롤기능

--- lib/main.dart
+++ lib/main.dart
@@ -75,50 +75,62 @@ class MyTest extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Container(
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 25, bottom: 25),
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '채팅',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.only(right: 20),
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                child: Icon(Icons.search, size: 30),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.only(right: 20),
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                child: Icon(Icons.forum, size: 30),
-                              ),
-                            ),
-                            MouseRegion(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 25, bottom: 25),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '채팅',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(right: 20),
+                            child: MouseRegion(
                               cursor: SystemMouseCursors.click,
-                              child: Icon(Icons.add_comment, size: 30),
+                              child: Icon(Icons.search, size: 30),
                             ),
-                          ],
-                        ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(right: 20),
+                            child: MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              child: Icon(Icons.forum, size: 30),
+                            ),
+                          ),
+                          MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: Icon(Icons.add_comment, size: 30),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
+                        ChatList(),
                       ],
                     ),
                   ),
-                  ChatList(),
-                  ChatList(),
-                  ChatList(),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
